0e6922ae9fdf75be520b223e6fe50dbb986fd496
Lucas Aguiar <lucascostaaguiar@hotmail.com>
2024-05-24 20:44:43 -0300

    devDojo spring

---
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -0,0 +1,17 @@
+package academy.devdojo.springboot2.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Anime {
+
+    private Long id;
+    private String name;
+
+
+}
