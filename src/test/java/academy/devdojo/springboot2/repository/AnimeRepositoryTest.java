0347ad6969f0f191f4b0116069897fad72c4463b
Lucas Aguiar <lucascostaaguiar@hotmail.com>
2024-08-08 23:35:19 -0300

    "RestTemplate new update "

---
+++ src/test/java/academy/devdojo/springboot2/repository/AnimeRepositoryTest.java
@@ -0,0 +1,16 @@
+package academy.devdojo.springboot2.repository;
+
+import org.junit.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class AnimeRepositoryTest {
+
+    @Test
+    public void test(){
+
+    }
+
+}
